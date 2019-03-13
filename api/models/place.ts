7fb61ac5c7d149bbf1257eca46bf83dcd8b69550
Luis <jose.luis.vidal.de.la.rosa@hp.com>
2019-03-13 10:15:32 +0100

    Defined model place

---
+++ api/models/place.ts
@@ -0,0 +1,21 @@
+"use strict";
+
+export type PlaceGroup = "monument" | "cathedral" | "garden" | "castle" | "palace" | "fountain" | "church" | "restaurant" | "hotel"; 
+export type PlaceTag = "sightseeing" | "eat" | "sleep" | "activity";
+
+export interface IBasicPlace {
+    id: string;
+    name: string;
+    description: string;
+    address: string;
+    image: string;
+    group: PlaceGroup;
+    tags: PlaceTag;
+}
+
+export interface IExtendedPlace extends IBasicPlace {
+    web?: string;
+    phone?: string;
+    thumbnail?: string;
+    gallery?: string[];
+}
